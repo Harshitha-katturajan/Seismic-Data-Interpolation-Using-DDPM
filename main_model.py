46a3a3b50db166a7ddd0cc25b149a1dc65ac7024
Harshitha-katturajan <katturajanharshitha@gmail.com>
2026-02-02 06:45:27 +0000

    Initial commit: Seismic Data Interpolation project

---
+++ main_model.py
@@ -0,0 +1,3 @@
+
+# Paste your main DDPM training/interpolation code here 
+# if you want the raw logic to be saved as a script.
---
+++ seismic_ddpm_main.py
@@ -0,0 +1,3 @@
+
+# PASTE YOUR ENTIRE MODEL CODE HERE AS TEXT
+# (Just the text of your imports, classes, and loops)
